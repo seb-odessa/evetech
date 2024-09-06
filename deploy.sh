9cdc0e3730d8c2b20c9f1a4389dc27e02fdbd13d
seb <seb@ukr.net>
2024-09-06 16:15:42 +0300

    Scripts updated

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ZKBINFO=vps
 BINARIES=$(find target/release -maxdepth 1 -type f -executable)
-SCRIPTS="scripts start.sh stop.sh *.db"
+SCRIPTS="scripts start.sh stop.sh"
 
 rsync -urP $SCRIPTS vps:~/zkbinfo/
 rsync -uP $BINARIES vps:~/zkbinfo/bin/
