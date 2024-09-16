2a5f8f45287a3f8c1881ca6a659938145e3722be
seb <seb@ukr.net>
2024-09-16 09:32:05 +0300

    watchdog updated

--- watchdog.sh
+++ watchdog.sh
@@ -2,13 +2,19 @@
 
 export ZKBINFO_HOST=185.87.51.139
 export ZKBINFO_PORT=8080
-export PROCESS_NAME="zkb_client"
-export CMD="/home/seb/zkbinfo/bin/${PROCESS_NAME}"
 
-if ! pgrep -f "$CMD" > /dev/null; then
-  echo "$(date): Процесс $PROCESS_NAME не найден. Запускаем..."
+export ZKB_CLI="/home/seb/zkbinfo/bin/zkb_client"
+export ZKBINFO="/home/seb/zkbinfo/bin/zkbinfo"
+
+if ! pgrep -f "$ZKBINFO" > /dev/null; then
+  echo "$(date): Процесс $ZKBINFO не найден. Выход..."
+  exit 0
+fi
+
+if ! pgrep -f "$ZKB_CLI" > /dev/null; then
+  echo "$(date): Процесс $ZKB_CLI не найден. Запускаем..."
   $CMD
 else
-  echo "$(date): Процесс $PROCESS_NAME уже запущен."
+  echo "$(date): Процесс $ZKB_CLI уже запущен."
 fi
 
