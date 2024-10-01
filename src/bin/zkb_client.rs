c2ccd298e3e6d7fb3ec4d2398080a95a6a6df4cf
seb <seb@ukr.net>
2024-10-01 11:33:21 +0300

    Improved zkb_client

--- src/bin/zkb_client.rs
+++ src/bin/zkb_client.rs
@@ -1,11 +1,99 @@
 use env_logger;
 use log::{error, info};
 use tokio::time::{sleep, Duration};
-use websockets::{Frame, WebSocket, WebSocketError};
+use websockets::{Frame, WebSocket};
+
+use evetech::killmails::killmail::Killmail;
 
 use std::env;
 
-// use evetech::killmail::Killmail;
+struct Processor {
+    api: String,
+    client: reqwest::Client,
+    payloads: Vec<String>,
+}
+impl Processor {
+    pub fn new(host: &String, port: u16) -> Self {
+        let api = format!("http://{host}:{port}/killmail/save");
+        Self {
+            api,
+            client: reqwest::Client::new(),
+            payloads: Vec::new(),
+        }
+    }
+
+    pub async fn handle(&mut self, frame: Frame) -> anyhow::Result<()> {
+        match frame {
+            Frame::Text {
+                payload,
+                continuation,
+                fin,
+            } => {
+                let len = payload.len();
+                info!("Frame::Text {{ {len}, {continuation}, {fin} }}",);
+                self.text(&payload, continuation, fin).await?
+            }
+            Frame::Binary {
+                payload,
+                continuation,
+                fin,
+            } => {
+                let len = payload.len();
+                info!("Frame::Binary {{ {len}, {continuation}, {fin} }}",);
+            }
+            Frame::Ping { payload } => {
+                info!("Frame::Ping {{ {:?} }}", payload);
+            }
+            Frame::Pong { payload } => {
+                info!("Frame::Ping {{ {:?} }}", payload);
+            }
+            Frame::Close { payload } => {
+                info!("Frame::Ping {{ {:?} }}", payload);
+            }
+        };
+        Ok(())
+    }
+
+    async fn text(
+        &mut self,
+        payload: &String,
+        continuation: bool,
+        fin: bool,
+    ) -> anyhow::Result<()> {
+        self.payloads.push(payload.clone());
+
+        if !continuation || fin {
+            let json = self.payloads.join("");
+            self.payloads.clear();
+
+            match serde_json::from_str::<Killmail>(&json) {
+                Ok(killmail) => {
+                    self.save(&killmail).await?;
+                }
+                Err(what) => {
+                    error!(": {what}");
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn save(&self, killmail: &Killmail) -> anyhow::Result<()> {
+        info!(
+            "killmail_id: {} {} {}",
+            killmail.killmail_id, killmail.solar_system_id, killmail.killmail_time
+        );
+
+        let res = self.client.post(&self.api).json(&killmail).send().await;
+        if res.is_err() {
+            sleep(Duration::from_secs(10)).await;
+            self.client.post(&self.api).json(&killmail).send().await?;
+        }
+
+        Ok(())
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,55 +107,18 @@ async fn main() -> anyhow::Result<()> {
     let api = format!("http://{host}:{port}/killmail/save");
     info!("zkbinfo API url: {api}");
 
+    let mut handler = Processor::new(&host, port);
+    info!("Handler created");
+
     let wss = "wss://zkillboard.com/websocket/";
     let enable = r#"{"action":"sub","channel":"killstream"}"#;
-
-    let client = reqwest::Client::new();
-    info!("Reqwest client created");
     let mut ws = WebSocket::connect(wss).await?;
     info!("Web Socket {:?} created", ws);
+
     ws.send_text(enable.to_string()).await?;
     info!("Web Socket request sent");
-
-    let mut parts = Vec::new();
     loop {
-        let maybe_response = ws.receive().await;
-        match maybe_response {
-            Ok(response) => {
-                if let Frame::Text {
-                    payload,
-                    continuation,
-                    fin,
-                } = response
-                {
-                    parts.push(payload);
-                    if !continuation || fin {
-                        let json = parts.join("");
-                        parts.clear();
-                        match serde_json::from_str::<evetech::killmails::killmail::Killmail>(&json) {
-                            Ok(killmail) => {
-                                info!("killmail_id: {}", killmail.killmail_id);
-                                let res = client.post(&api).json(&killmail).send().await;
-                                if res.is_err() {
-                                    sleep(Duration::from_secs(10)).await;
-                                    client.post(&api).json(&killmail).send().await?;
-                                }
-                            }
-                            Err(what) => {
-                                error!(": {what}");
-                            }
-                        }
-                    }
-                }
-            }
-            Err(what) => {
-                error!("Web Socket {what}");
-                if let WebSocketError::ReadError(_) = what {
-                    ws.close(None).await?;
-                    ws = WebSocket::connect(wss).await?;
-                    ws.send_text(enable.to_string()).await?;
-                }
-            }
-        }
+        let frame = ws.receive().await?;
+        handler.handle(frame).await?
     }
 }
